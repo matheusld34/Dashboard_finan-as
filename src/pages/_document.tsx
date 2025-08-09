8fc468a87ed216956efea929522ad0dd4dc4e3e5
Matheus <matheus_lopes151@hotmail.com>
2025-08-09 17:48:29 -0300

    primeiro commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { cn } from "@/lib/utils";
+import { Sidebar } from "@/components/sidebar";
+
+//import { Inter } from "next/font/google"
+
+
+//const inter = Inter({ subsets: ["latin"] })
 
 export default function Document() {
   return (
+
     <Html lang="en">
+
       <Head />
-      <body className="antialiased">
+
+
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        //inter.className
+      )}>
+
         <Main />
+
         <NextScript />
+
       </body>
     </Html>
   );
