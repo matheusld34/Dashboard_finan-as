e8697a74083d627c8c3d0ebba81e0228622e21e0
Matheus <matheus_lopes151@hotmail.com>
2025-08-10 15:22:53 -0300

    cards

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Package, PanelBottom, Settings2, ShoppingBag, Users } from "lucide-react";
+import { Package, PanelBottom, Settings2, ShoppingBag, Users, Home, LogOut } from "lucide-react";
 import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@radix-ui/react-tooltip";
 
 
@@ -9,17 +9,101 @@ export function Sidebar() {
     return (
         <div className=" flex w-full flex-col bg-muted/40">
 
-            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex">
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
                 <nav className=" flex flex-col items-center gap-4 px-2 py-5">
                     <TooltipProvider>
                         <Link
                             href="#"
                             className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
                         >
-                            <Package className="h-4 w-4" />
+                            <Package className="h-5 w-5" />
                             <span className="sr-only">Dashboard Avatar</span>
                         </Link>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <Home className="h-5 w-5" />
+                                    <span className="sr-only">Início</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Início</TooltipContent>
+
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <Home className="h-5 w-5" />
+                                    <span className="sr-only">Pedidos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Pedidos</TooltipContent>
+
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <Package className="h-5 w-5" />
+                                    <span className="sr-only">Produtos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Produtos</TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <Users className="h-5 w-5" />
+                                    <span className="sr-only">Clientes</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Clientes</TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <Settings2 className="h-5 w-5" />
+                                    <span className="sr-only">Configureções</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Configureções</TooltipContent>
+                        </Tooltip>
+
+
                     </TooltipProvider>
+                </nav>
+                <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                                >
+                                    <LogOut className="h-5 w-5 text-red-500" />
+                                    <span className="sr-only">Sair</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Sair</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
                 </nav>
             </aside>
 
